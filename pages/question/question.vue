8a60e771b69397bc9fca4e109c24d6e31b23b28a
liuxiaodou123 <359311427@qq.com>
2019-08-19 09:34:07 +0800

    wxss

--- pages/question/question.vue
+++ pages/question/question.vue
@@ -1,5 +1,5 @@
 <template>
-	<view style="width:100%;height: 100%;background: #F2F2F2;">
+	<view class="pdpage160">
 		<!-- search部分 -->
 		<view class="search flex flexCenter">
 			<input class="searchword" type="text" placeholder="请输入关键词"/>
@@ -9,7 +9,7 @@
 		</view>
 		<!-- nav部分 -->
 		<view>
-			<ul class="nav flex">
+			<ul class="qustNav flex">
 				<li><span class="nav_actived">咨询</span></li>
 				<li><span>投诉</span></li>
 				<li><span>建议</span></li>
@@ -54,6 +54,7 @@
 				</view>
 			</view>
 			
+			
 		</view>
 		<c-tabbar></c-tabbar>
 	</view>
@@ -79,22 +80,5 @@
 <style scoped>
 	@import url("../../assets/style/public.css");
 	@import url("../../assets/style/question.css");
-	/* search部分 */
-	.search{width: 100%;height: 100rpx;background: #f5f5f5;}
-	.searchword{width: 690rpx;height: 60rpx;font-size: 24rpx;border: solid 1px #999999;text-indent: 20rpx;border-radius: 10rpx;position: relative;}
-	.searchIcon{width: 30rpx;height: 30rpx;position: absolute;right: 8%;}
-	/* nav部分 */
-	.nav{font-size: 28rpx;width: 100%;background: #FFFFFF;border: solid 1px #EFEFEF;height: 100rpx;}
-	.nav>li{-webkit-flex: 1;text-align: center;color: #666666;}
-	.nav>li>span{padding: 16rpx 0;}
-	.nav_actived{line-height: 100%;color: #1095f9;border-bottom: solid 6px #1095F9;}
-	/* content部分 */
-/* 	.content{flex-direction:column;padding-bottom: 98rpx;}
-	.content_box{width: 94%;height:306rpx;background: #FFFFFF;box-shadow: 0px 0px 12rpx rgba(0,0,0,0.1); border-radius: 10rpx; padding-right: 10rpx; box-sizing: border-box; margin:10rpx auto;}
-	.content_box_question{display: flex;}
-	.content_box_question_left{padding: 0 20rpx;float: left;height: 100%;}
-	.content_box_question_left>image{width: 28rpx;height: 24rpx;}
-	.content_box_question_right{float: left;font-size: 26rpx;}
-	.response{color: #666666;font-size: 24rpx;}
-	.lookdetail{font-size: 22rpx;color: #1095F9;text-align: right;} */
+
 </style>
